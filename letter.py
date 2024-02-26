4fee0b933224bed55d3124d4cd034537175a542f
Kaustubh <kaustubh_awasthi@proton.me>
2024-02-26 09:39:52 +0530

    programs

---
+++ letter.py
@@ -0,0 +1,6 @@
+phrase1 = "SATI"
+phrase2 = "cse"
+print(phrase1)
+print(phrase2.upper())
+print(phrase2.islower())
+print(phrase2.upper().isupper())
---
+++ variable.py
@@ -0,0 +1,6 @@
+name = "Jhon"
+age = '70'
+print("There once was a man named " + name + ",")
+print("He was " + age + " years old.")
+print("He liked the name " + name + ",")
+print("but disliked being " + age + " years old.")
