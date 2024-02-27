d08bf4f20755353d64605d47b6be70225b0eb77e
Kaustubh <kaustubh_awasthi@proton.me>
2024-02-27 12:20:15 +0530

    ...

--- letter.py
+++ letter.py
@@ -1,6 +1,13 @@
 phrase1 = "SATI"
 phrase2 = "cse"
 print(phrase1)
+# Convert to uppercase
 print(phrase2.upper())
+# check if it is lowercase
 print(phrase2.islower())
 print(phrase2.upper().isupper())
+
+# Gives the lenght
+print(len(phrase1))
+# Gives the specific letter through index
+print(phrase1[2])
---
+++ temp.py
@@ -0,0 +1,9 @@
+# Convert Celsius to Fahrenheit
+C = 32
+print( "F = " + str(9*C/5 + 32))
+# Here str is used as integer can't be added with string so we convert the integer to string
+
+# Anothe way to do it
+# Convert Fahrenheit to Celsius
+F = 32
+print("C =", (F-32)*5/9)
