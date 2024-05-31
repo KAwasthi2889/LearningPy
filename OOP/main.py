035f675af05a872e6e5719f6f464f2535b6ef2d8
Kaustubh <kaustubh_awasthi@proton.me>
2024-05-31 19:21:33 +0530

    ...

--- OOP/main.py
+++ OOP/main.py
@@ -3,7 +3,6 @@
 '''
 import will also run the progam basic.py
 '''
-
 import csv
 
 class Item:
@@ -21,7 +20,7 @@ class Item:
         def total_price(self):
             return self.quantity * self.price * (1 - self.discount/100)
         
-        @classmethod # Classmethod can only be called from class
+        @classmethod # Classmethod can only be called from class. @<decorator>
         def from_csv(cls):
             with open('OOP/items.csv', 'r') as file:
                 content = csv.DictReader(file) # arranging content as dictionary
@@ -33,7 +32,15 @@ class Item:
                         quantity = int(item.get('quantity')) 
                     )
 
-        
+        # StaticMethod makes a function instead of method. num is a parameter here instead of instace
+        @staticmethod
+        def is_integer(num):
+            if isinstance(num, float): # Checks any new instance if it is float
+                return num.is_integer() # if num is float it will check
+            elif isinstance(num, int):
+                return True
+            else:
+                return False
         def __repr__(self):
             # tells how to reprsent the informationin in print
             return f"Item({self.name}, {self.price}, {self.quantity})"
@@ -49,6 +56,5 @@ item4 = item('Cable', 15, 10)
 item5 = item('Mouse', 5, 8)
 '''
 print(Item.all)
-
-
+print(Item.is_integer(7))
 
